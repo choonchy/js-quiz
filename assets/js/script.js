d6e5b31e5aafbb077b5cea845c3912daf2e18e24
Tom Chappell <thomas.chappell@outlook.com>
2021-03-29 02:04:29 +1100

    finalising all

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,115 @@
-var playerScore = 0;
+const startButton = document.getElementById('start-btn');
+const answerButton = document.getElementById('answer-btn');
+const questionContainer = document.getElementById('question-container');
+const questionEl = document.getElementById('question');
+const aAnswer = document.getElementById("answer-a")
+const bAnswer = document.getElementById("answer-b")
+const cAnswer = document.getElementById("answer-c")
+const scoreEl = document.getElementById("score")
+const timerEl = document.getElementById("timer")
+const endGameEl = document.getElementById("endgame")
+const highScoreEl = document.getElementById("high-scores")
+const playerScoreEl = document.getElementById("player-score")
 
-var timerLength = 600;
+var timer = 60;
+var questionIndex = 0;
+var score = 0;
+var timerInterval;
 
-var countdown = function(num) {
-    for (var i = num; i > 0; i--) {
-        console.log(i);
+function startQuiz() {
+    console.log('started')
+    startButton.classList.add('hide');
+    questionContainer.classList.remove('hide');
+    quiz()
+}
+
+function countdown(num){
+    timer = num;
+    timerEl.textContent = "Time left: " + num
+    timerInterval = setInterval(function() {
+    timer--;
+    timerEl.textContent = "Time left: " + timer;
+    if (timer < 0) {
+        clearInterval(timerInterval);
+        endQuiz()
+    } 
+    }, 1000)
+}
+
+
+function quiz(){
+    countdown(timer);
+    scoreEl.textContent = "Score: " + score
+    questionEl.textContent = questionsList[questionIndex].question
+    aAnswer.textContent = questionsList[questionIndex].answers.a
+    bAnswer.textContent = questionsList[questionIndex].answers.b
+    cAnswer.textContent = questionsList[questionIndex].answers.c
+}
+
+function nextQuestion() {
+    if(questionIndex < (questionsList.length - 1)) {
+        questionIndex++
+        clearInterval(timerInterval)
+        quiz()
+    } else if(questionIndex === 5) {
+        endQuiz()
     }
-};
+}
 
-countdown(timerLength);
+function answerSelectedA() {
+    if (questionsList[questionIndex].answers.a === questionsList[questionIndex].correctAnswer) {
+        console.log('Correct!')
+        score = score + 100 + (10 * timer)
+        nextQuestion()
+    } else {
+        console.log('Incorrect')
+        nextQuestion()
+    }
+}
 
+function answerSelectedB() {
+    if (questionsList[questionIndex].answers.b === questionsList[questionIndex].correctAnswer) {
+        console.log('Correct!')
+        score = score + 100 + (10 * timer)
+        nextQuestion()
+    } else {
+        console.log('Incorrect')
+        timer = timer - 5;
+        nextQuestion()
+    }
+}
 
-var questions = [
+function answerSelectedC() {
+    if (questionsList[questionIndex].answers.c === questionsList[questionIndex].correctAnswer) {
+        console.log('Correct!')
+        score = score + 100 + (10 * timer)
+        nextQuestion()
+    } else {
+        console.log('Incorrect')
+        nextQuestion()
+    }
+}
+
+function endQuiz() {
+    var playerName = prompt("Enter your initials!");
+    questionContainer.classList.add('hide');
+    playerScoreEl.textContent = "You finished! Your score is: " + score
+    for (i = 0; i < highScores.length; i++) {
+        if (score < highScores[i].score){
+            
+        } else {
+            var highScore = document.createElement("li")
+            highScoreEl.appendChild(highScore)
+        }
+    }
+}
+
+startButton.addEventListener('click', startQuiz)
+aAnswer.addEventListener('click', answerSelectedA)
+bAnswer.addEventListener('click', answerSelectedB)
+cAnswer.addEventListener('click', answerSelectedC)
+
+var questionsList = [
     {
         question: "What value is given to an empty variable?",
         answers: {
@@ -19,7 +117,7 @@ var questions = [
             b: "Empty",
             c: "Nil",
         },
-        correctAnswer: "a"
+        correctAnswer: "Null"
     },
     {
         question: "Which of these is not a JS library?",
@@ -28,7 +126,7 @@ var questions = [
             b: "React",
             c: "Python",
         },
-        correctAnswer: "c"
+        correctAnswer: "Python"
     },
     {
         question: "What does 'var' stand for in JavaScript?",
@@ -37,7 +135,7 @@ var questions = [
             b: "Variable",
             c: "Variety",
         },
-        correctAnswer: "b"
+        correctAnswer: "Variable"
     },
     {
         question: "What year did JavaScript first appear?",
@@ -46,7 +144,7 @@ var questions = [
             b: "2001",
             c: "1997",
         },
-        correctAnswer: "a"
+        correctAnswer: "1995"
     },
     {
         question: "What character is used to 'escape' special characters (such as quotation marks)?",
@@ -55,7 +153,7 @@ var questions = [
             b: "Forwardslash",
             c: "Ampersand",
         },
-        correctAnswer: "a"
+        correctAnswer: "Backslash"
     },
     {
         question: "What does the 'pop' array method do?",
@@ -64,6 +162,29 @@ var questions = [
             b: "Adds a new element to the array and returns the array's length",
             c: "Removes the last element in the array and returns its value",
         },
-        correctAnswer: "c"
+        correctAnswer: "Removes the last element in the array and returns its value"
+    },
+]
+
+var highScores = [
+    {
+        name: "TC",
+        score: 0
+    },
+    {
+        name: "TC",
+        score: 0
+    },
+    {
+        name: "TC",
+        score: 0
+    },
+    {
+        name: "TC",
+        score: 0
+    },
+    {
+        name: "TC",
+        score: 0
     },
 ]
